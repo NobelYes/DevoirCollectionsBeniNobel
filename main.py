4fc5c0898a1835c9a629efe69989331f923e4a02
NobelYes <b.bazombanza@gmail.com>
2023-05-09 11:01:35 +0200

    affichage de elements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 if __name__=="__main__":
     # Question 1
-    # 1
         marque_vehicules=['Honda','Toyota','Mercedes','ferrari','lambourghini','Mazda','Citroen','Audi','BMW','Peugeot']
-        
+    #    1 affichage des elements de la liste
+        print(marque_vehicules)
