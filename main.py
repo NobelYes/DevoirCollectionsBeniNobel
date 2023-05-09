7b0e5e5c5fdaac12687ff0aade6be6280c5be48d
NobelYes <b.bazombanza@gmail.com>
2023-05-09 10:57:22 +0200

    creation de la liste

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__=="__main__":
+    # Question 1
+    # 1
+        marque_vehicules=['Honda','Toyota','Mercedes','ferrari','lambourghini','Mazda','Citroen','Audi','BMW','Peugeot']
+        
