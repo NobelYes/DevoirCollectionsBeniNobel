c243e2f631c760ab1d5eb5a4d99a769dc45d5c87
NobelYes <b.bazombanza@gmail.com>
2023-05-09 11:04:23 +0200

    modification de la valeur de l'elt nr 5

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ if __name__=="__main__":
         marque_vehicules=['Honda','Toyota','Mercedes','ferrari','lambourghini','Mazda','Citroen','Audi','BMW','Peugeot']
     #    1 affichage des elements de la liste
         print(marque_vehicules)
+        # 2 changer le contenu de l'element numero 5
+        marque_vehicules[4]='Tesla'
+        # affichage de la liste actualiser
+        print(marque_vehicules)
